1cd2ef0f771b569e7532fce8c5125d9bb1e529e5
Fillipe Miguel <Fillipeolipe@gmail.com>
2023-02-08 08:11:32 -0300

    Aula 07

--- src/Components/projects/ProjectForm/index.jsx
+++ src/Components/projects/ProjectForm/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Components
 import Input from "../../form/Input";
@@ -9,6 +9,22 @@ import Submit from "../../form/Submit";
 import style from "./style.module.css";
 
 const ProjectForm = ({ btnText }) => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
     return (
         <form className={style.form}>
             <Input
@@ -23,7 +39,11 @@ const ProjectForm = ({ btnText }) => {
                 name="budget"
                 placeholder="Insira o orçamento total"
             />
-            <Select name="category_id" text="Selecione uma categoria" />
+            <Select
+                name="category_id"
+                text="Selecione uma categoria"
+                options={categories}
+            />
             <Submit text={btnText} />
         </form>
     );
